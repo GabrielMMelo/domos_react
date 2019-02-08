9a6a4ac73966e8c7ff0087c10a0968f1aa62faec
Gabriel Marques de Melo <gabrielmelocomp@gmail.com>
2019-02-08 18:39:14 -0200

    Added javascript WebSocket handler

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -29,8 +29,8 @@ class Dashboard extends Component {
             <div className="row justify-content-center mt-4">
                 {this.state.data.map( (node) => {
                     return (
-                        <div className="col-3" key={node.name}>
-                            <Node image={node.image} name={node.name} purpouse={node.purpouse} is_active={node.is_active} />
+                        <div className="col-3" key={node.id}>
+                            <Node image={node.image} id={node.id} name={node.name} purpouse={node.purpouse} is_active={node.is_active} />
                         </div>
                     );
                 })}
--- src/components/Node/index.js
+++ src/components/Node/index.js
@@ -4,19 +4,57 @@ import { Card, CardImg, CardText, CardBody,
   CardTitle, CardSubtitle, Button } from 'reactstrap';
 
 class Node extends Component { 
-  render() {
-    return (
-        <Card>
-            <CardBody className="shadow-sm">
-                <CardImg top width="100%" src={this.props.image} alt={this.props.name + " image"} />
-                <CardTitle><h2>{this.props.name}</h2></CardTitle>
-                <CardSubtitle className="text-muted"><h5>{this.props.purpouse}</h5></CardSubtitle>
-                <CardText>State: <span>{this.props.is_active ? "On" : "Off"}</span></CardText>
-                <Button>Toggle</Button>
-            </CardBody>
-        </Card>
-    );
-  }
+    constructor(props){
+        super(props);
+        this.state = {
+            is_active: props.is_active,
+        };
+
+
+        this.nodeSocket = new WebSocket(
+            'ws://192.168.0.110:8000/ws/node/node_' +
+            props.id + '/');
+
+        // Bind
+        this.toggleState = this.toggleState.bind(this);
+    }
+
+    componentDidMount() {
+        this.nodeSocket.onmessage = function(e) {
+            let data = JSON.parse(e.data);
+            let message = data['message'];
+            this.setState( {is_active: message} );
+        }.bind(this);
+        
+        this.nodeSocket.onclose = function(e) {
+            console.error('Node socket closed unexpectedly');
+        };
+    };
+
+    toggleState() {
+        this.setState({
+            is_active: !this.state.is_active
+            },  () =>  { 
+                this.nodeSocket.send(JSON.stringify({
+                    'message': this.state.is_active
+                }));
+            }
+        );
+    };
+
+    render() {
+        return (
+            <Card>
+                <CardBody className="shadow-sm">
+                    <CardImg top width="100%" src={this.props.image} alt={this.props.name + " image"} />
+                    <CardTitle><h2>{this.props.name}</h2></CardTitle>
+                    <CardSubtitle className="text-muted"><h5>{this.props.purpouse}</h5></CardSubtitle>
+                    <CardText>State: <span>{this.state.is_active ? "On" : "Off"}</span></CardText>
+                    <Button onClick={this.toggleState}>Toggle</Button>
+                </CardBody>
+            </Card>
+        );
+    }
 }
 
 export default Node;
