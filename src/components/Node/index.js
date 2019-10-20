e79244d0fd3d781b80bf2a3181b2fcc728f2d430
GabrielMMelo <gabrielmelocomp@gmail.com>
2019-10-20 09:29:20 -0300

    Incorporating material-ui to project

--- src/components/Node/index.js
+++ src/components/Node/index.js
@@ -3,6 +3,9 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody,
   CardTitle, CardSubtitle, Button } from 'reactstrap';
 
+import Box from '@material-ui/core/Box'
+import Typography from '@material-ui/core/Typography'
+
 const wsServerHost = 'gabrielmelo.ddns.net:8081';
 
 class Node extends Component {
@@ -10,16 +13,24 @@ class Node extends Component {
         super(props);
         this.state = {
             is_active: props.is_active,
+            wsConnected: false,
         };
 
         this.nodeSocket = new WebSocket(
-            'ws://' + wsServerHost + '/ws/device/' + props.id + '/');
+            'ws://' + wsServerHost + '/ws/device/' + props.id + '/'
+        );
+
+        //TODO: fire a message through ws connection to authorize
+
+        this.setState({ wsConnected: true });
 
         // Bind
         this.toggleState = this.toggleState.bind(this);
     }
 
     componentDidMount() {
+        const { wsConnected } = this.state;
+
         this.nodeSocket.onmessage = function(e) {
             let data = JSON.parse(e.data);
             let state = parseInt(data['state']);
@@ -28,6 +39,7 @@ class Node extends Component {
         }.bind(this);
 
         this.nodeSocket.onclose = function(e) {
+            this.setState({ wsConnected: false });
             console.error('Node socket closed unexpectedly');
         };
     };
@@ -46,6 +58,9 @@ class Node extends Component {
 
     //<CardImg top width="100%" src={this.props.image} alt={this.props.name + " image"} />
     render() {
+
+        const { wsConnected } = this.state;
+
         return (
             <Card>
                 <CardBody className="shadow-sm">
@@ -53,6 +68,9 @@ class Node extends Component {
                     <CardSubtitle className="text-muted"><h5>{this.props.type}</h5></CardSubtitle>
                     <CardText>State: <span>{this.state.is_active ? "On" : "Off"}</span></CardText>
                     <Button onClick={this.toggleState}>Toggle</Button>
+                    <Box align="right">
+                        <Typography onClick={this.toggleState}>Toggle</Typography>
+                    </Box>
                 </CardBody>
             </Card>
         );
