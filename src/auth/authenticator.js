fb6da8801e2b3e4e2d8f51543b89e9c83c737fb0
Gabriel Marques de Melo <gabrielmelocomp@gmail.com>
2019-11-01 20:12:33 -0300

    Added AES to token

--- src/auth/authenticator.js
+++ src/auth/authenticator.js
@@ -1,4 +1,5 @@
 import cookie from 'react-cookies';
+import aesjs from 'aes-js';
 
 import api from '../services/api';
 
@@ -10,9 +11,14 @@ export const login = async (email, password) => {
 
     await api.post("auth/login/", body)
         .then(res => {
+            const token = res.data['key'];
             const expires = new Date();
             expires.setDate(Date.now() + 1000 * 60 * 60 * 24 * 14);
-            cookie.save('token', res.data['key'], {
+            let tokenBytes = aesjs.utils.utf8.toBytes(token);
+            var aesCtr = new aesjs.ModeOfOperation.ctr(process.env.REACT_APP_CTRK.split('').map((i)=>Number(i)), new aesjs.Counter(5));
+            let encryptedBytes = aesCtr.encrypt(tokenBytes);
+            var encryptedHex = aesjs.utils.hex.fromBytes(encryptedBytes);
+            cookie.save('token', encryptedHex, {
                 path: '/',
                 expires,
                 //httpOnly: true,
@@ -21,8 +27,32 @@ export const login = async (email, password) => {
         })
 };
 
+export const getUserInfo = () => {
+    api.get("auth/user/", { headers: { Authorization: `Token ${getToken()}` } }) 
+        .then((res) => {
+            const expires = new Date();
+            expires.setDate(Date.now() + 1000 * 60 * 60 * 24 * 14);
+            cookie.save('firstName', res.data['first_name'], {
+                path: '/',
+                expires,
+                //httpOnly: true,
+                //secure: true,
+            })
+        });
+}
+
+export const getFirstName = () => {
+    return cookie.load('firstName');
+}
+
 export const getToken = () => {
-    return cookie.load('token');
+    let encryptedToken = cookie.load('token');
+    var encryptedBytes = aesjs.utils.hex.toBytes(encryptedToken);
+    var aesCtr = new aesjs.ModeOfOperation.ctr(process.env.REACT_APP_CTRK.split('').map((i)=>Number(i)), new aesjs.Counter(5));
+    var decryptedBytes = aesCtr.decrypt(encryptedBytes);
+    var decryptedToken = aesjs.utils.utf8.fromBytes(decryptedBytes);
+    return decryptedToken;
+
 };
 
 export const logout = async () => {
